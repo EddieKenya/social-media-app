5de485078d07dfea1648948f02c2213b702d164e
Edwin Makhanu <edwinmakhanu630@gmail.com>
2022-11-15 16:23:05 +0300

    login

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from.import views
 
 urlpatterns = [
-    path('signup/', views.sign_up, name='signup'),
     path('',views.home, name='home'),
+    path('signup/', views.sign_up, name='signup'),
+    path('login/', views.login, name='login')
     
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
-from django.contrib import messages
+from django.contrib import messages,auth
+from.models import *
 
 # Create your views here.
 def sign_up(request):
@@ -26,21 +27,39 @@ def sign_up(request):
                 new_user.first_name = firstname
                 new_user.last_name = lastname
                 new_user.save()
-                return redirect('home')
 
-
-                
-
+                user_model = User.objects.get(username=username)
+                user_profile = Authentication.objects.create(user=user_model, id_user = user_model.id) 
+                return redirect('login')
         
         else:
             messages.info(request, 'Password Not Matching')
             return redirect ('signup')
             
-
-
-
     else:
         return render(request, 'signup.html')
     
 def home (request):
     return render (request, 'home.html')
+
+def login (request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST ['password']
+
+        
+
+        user = auth.authenticate(username =username, password =password)
+
+        if user is not None:
+            auth.login( request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'credentials Invalid')
+            return redirect ('login')
+     
+
+
+    else:
+        return render(request, 'login.html')
+   
