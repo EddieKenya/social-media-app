ac229d8676f2b33e72a4a2d4596ee901ff1a73d7
Edwin Makhanu <edwinmakhanu630@gmail.com>
2022-12-28 04:33:25 +0300

    railway deploy

--- family/settings.py
+++ family/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-rd0%l)_d-dkf0q)naf6tf@ai23#-k&*0cq1uz7)+wr3$!hw))7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,9 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
