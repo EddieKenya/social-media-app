90f8f56bbbd70f4748847f78966fa754552095cd
Edwin Makhanu <edwinmakhanu630@gmail.com>
2022-12-28 15:28:59 +0300

    csrf tokens

--- family/settings.py
+++ family/settings.py
@@ -134,3 +134,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
+CSRF_TRUSTED_ORIGINS = ['https://web-production-34cc.up.railway.app']
